309bd73d4f8aea8afc8f716b25849f8e627c972e
Hamuj12 <69257879+Hamuj12@users.noreply.github.com>
2023-01-24 17:47:59 -0600

    updated student_names.py

--- Student_Names.py
+++ Student_Names.py
@@ -1,8 +1,17 @@
+# s = open('studentnames.txt', 'r')
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# s.close()
+
 s = open('studentnames.txt', 'r')
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
+n = 0
+for line in s:
+    print(line.rstrip('\n'))
+    n += 1
+
+print('There were', n-1, 'students in the file.')
 s.close()
